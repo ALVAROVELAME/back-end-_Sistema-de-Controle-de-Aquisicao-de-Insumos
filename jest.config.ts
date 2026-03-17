9765ae2dd08d7259aed70dc02b394bd592b54251
ALVAROVELAME <alvarovelame@gmail.com>
2026-03-17 15:21:43 -0300

    refactor: adiciona build com dist e melhora gitignore e dockerignore

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,19 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
+
   testMatch: ["**/tests/**/*.test.ts"],
+
   moduleFileExtensions: ["ts", "js"],
-  verbose: true
+
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+
+  verbose: true,
+
+  // 🔥 evita qualquer output físico
+  cache: false
 };
 
 export default config;
